67e4e9e39dd424e6c07bd91d8b18d47b52110437
very-amused <very-amused@pm.me>
2025-01-17 13:20:19 -0500

    Support bootstrapping user files

--- bootstrap.go
+++ bootstrap.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"path"
 )
 
 // BootstrapConfig - Configuration for bootstrapping jail(s) (jail-{name}-bootstrap.sh) or a FreeBSD image (img-bootstrap.sh)
@@ -31,12 +32,14 @@ type BootstrapUser struct {
 	Shell string // Shell command for user (default: "sh" [set by FreeBSD]) NOTE: this different from Linux. FBSD wants an executable name, NOT a full path
 
 	CreateHome *bool       // Whether to make a home directory for the user (default: true)
-	HomeDir    string      // Location for user's home directory (default: "/home/{{.Name}}" [set by FreeBSD])
+	HomeDir    string      // Location for user's home directory (default: "/home/{{.Name}}")
 	HomePerms  os.FileMode // User homedir permissions (default: 0o700)
 
 	GenPasswd bool // Whether to let FreeBSD generate a random password instead of prompting for a user-supplied password. (default: false)
 	// NOTE: If GenPasswd is set in an *image* bootstrap config, each *jail* deployed from that image will receive its own unique password generated for that user.
 	// Bootstrap user accounts are *locked* on images to ensure password security across different jails.
+
+	Files []string // List of files to copy from the host to the user's homedir in the jail.
 }
 
 // Set default values where applicable
@@ -44,6 +47,9 @@ func (u *BootstrapUser) setDefaults() {
 	if u.CreateHome == nil {
 		u.CreateHome = new(bool)
 		*u.CreateHome = true
+	}
+	if u.HomeDir == "" {
+		u.HomeDir = path.Join("/home", u.Username)
 	}
 	if u.HomePerms == 0 {
 		u.HomePerms = 0o700
